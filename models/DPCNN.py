fcd204807a9db73243c77e8828e42f48b7c13842
YunshengPang <434497112@qq.com>
2020-05-02 09:55:03 +0800

    add test for DPCNN

--- models/DPCNN.py
+++ models/DPCNN.py
@@ -8,6 +8,7 @@ class Config(object):
     def __init__(self):                                    
         self.dim_embed = 300
         self.num_filters = 256
+        self.kernel_size = 3
 
 
 class DPCNN(nn.Module):
@@ -15,13 +16,14 @@ class DPCNN(nn.Module):
         super(DPCNN, self).__init__()
         self.dim_embed = config.dim_embed
         self.num_filters = config.num_filters
+        self.kernel_size = config.kernel_size
         self.n_vocab = vocab_size
         self.num_classes = num_classes
 
         self.embedding = nn.Embedding(self.n_vocab, self.dim_embed)
 
-        self.conv_region = nn.Conv2d(1, self.num_filters, (3, self.dim_embed), stride=1)
-        self.conv = nn.Conv2d(self.num_filters, self.num_filters, (3, 1), stride=1)
+        self.conv_region = nn.Conv2d(1, self.num_filters, (self.kernel_size, self.dim_embed), stride=1)
+        self.conv = nn.Conv2d(self.num_filters, self.num_filters, (self.kernel_size, 1), stride=1)
         self.max_pool = nn.MaxPool2d(kernel_size=(3, 1), stride=2)
         self.padding1 = nn.ZeroPad2d((0, 0, 1, 1))  # top bottom
         self.padding2 = nn.ZeroPad2d((0, 0, 0, 1))  # bottom
@@ -29,20 +31,20 @@ class DPCNN(nn.Module):
         self.fc = nn.Linear(self.num_filters, self.num_classes)
 
     def forward(self, x):
-        batch_size = x.shape[0]
-
-        x = self.embedding(x)
-        x = x.unsqueeze(1)
-        x = self.conv_region(x)
-        x = self.padding1(x)
+        x = self.embedding(x)     # [batch_size, max_len, dim_embed]
+        x = x.unsqueeze(1)        # [batch_size, 1, max_len, dim_embed]
+        x = self.conv_region(x)   # [batch_size, num_filters, max_len-kernel_size, 1]
+        x = self.padding1(x)      # [batch_size, num_filters, max_len, 1]
         x = self.relu(x)
-        x = self.conv(x)
-        x = self.padding1(x)
+        x = self.conv(x)          # [batch_size, num_filters, max_len-kernel_size, 1]
+        x = self.padding1(x)      # [batch_size, num_filters, max_len, 1]
         x = self.relu(x)
-        x = self.conv(x)
+        x = self.conv(x)          # [batch_size, num_filters, max_len-kernel_size, 1]
         while x.size()[2] > 2:
-            x = self._block(x)
-
+            x = self._block(x)    # [batch_size, num_filters, 1, 1]
+        x = x.squeeze()           # [batch_size, num_filters]
+        x = self.fc(x)            # [batch_size, num_classes]
+         
         return x
 
     def _block(self, x):
@@ -64,13 +66,14 @@ class DPCNN(nn.Module):
 
 if __name__ == "__main__":
     config = Config()
-    dpcnn = DPCNN(config, 500, 2)
-
     max_len = 100
     batch_size = 8
+    vocab_size = 500
+    num_classes = 2
 
-    x = torch.LongTensor(batch_size, max_len).random_(0, 2000)
-    print(x.size())  # [8, 100]
+    dpcnn = DPCNN(config, vocab_size, num_classes)
+
+    x = torch.LongTensor(batch_size, max_len).random_(0, vocab_size)  # [8, 100]
 
     x = dpcnn(x)
     print(x.size())
